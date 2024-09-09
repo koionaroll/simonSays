c7e27104c18e118f83b2b52c03e8534ed1764147
Khoi Tran <tranvankhoi2002@gmail.com>
2024-09-09 16:02:03 -0700

    simon turn

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
     justify-content: left;
     margin: 1rem;
   `;
-  const Button = styled.button`
+  const Button = styled.button<{isLit?:boolean;}>`
     background: transparent;
     border-radius: 1rem;
     border-width: 0.25rem;
@@ -41,6 +41,7 @@ function App() {
 
     &:nth-child(2) {
       background: #214c7a;
+      background : ${props => props.isLit === true? "#3171b5" : "#214c7a"};
       &:hover {
         cursor: pointer;
         background: #3171b5;
@@ -48,6 +49,7 @@ function App() {
     }
     &:nth-child(4) {
       background: #777a21;
+      background : ${props => props.isLit === true? "#a6b531" : "#777a21"};
       &:hover {
         cursor: pointer;
         background: #a6b531;
@@ -55,6 +57,7 @@ function App() {
     }
     &:nth-child(6) {
       background: #7a2121;
+      background : ${props => props.isLit === true? "#b53131" : "#7a2121"};
       &:hover {
         cursor: pointer;
         background: #b53131;
@@ -62,21 +65,31 @@ function App() {
     }
     &:nth-child(8) {
       background: #217a34;
+      background : ${props => props.isLit === true? "#33b531" : "#217a34"};
       &:hover {
         cursor: pointer;
         background: #33b531;
       }
     }
   `;
+
   const Start = styled.h1`
     cursor: pointer;
   `;
+
   const [score, setScore] = useState(0);
   const [highscore, setHighscore] = useState(0);
   const [hideStart, setHideStart] = useState(true);
   const [simonTurn, setSimonTurn] = useState(false);
+  const [playerTurn, setPlayerTurn] = useState(false);
   const [round, setRound] = useState(0);
   const [randSeq, setRandSeq] = useState<number[]>([]);
+  const [isLitOne, setIsLitOne] = useState(false);
+  const [isLitTwo, setIsLitTwo] = useState(false);
+  const [isLitThree, setIsLitThree] = useState(false);
+  const [isLitFour, setIsLitFour] = useState(false);
+  // const [playerSeq, setPlayerList] = useState<number[]>([]);
+
 
   useEffect(() => {
     const generateSeed = Array.from(
@@ -89,8 +102,9 @@ function App() {
   const startGame = () => {
     setHideStart(!hideStart);
     console.log(randSeq);
+    console.log(randSeq);
     setSimonTurn(!simonTurn);
-    setRound(round + 1);
+    setRound(round + 4);
   };
 
   useEffect(() => {
@@ -99,29 +113,74 @@ function App() {
     }
   }, [simonTurn, round]);
 
+  useEffect(() => {
+    if (playerTurn) {
+      runPlayerTurn(playerTurn, round, runSimonTurn(simonTurn, round));
+    }
+  }, [playerTurn, round]);
+
   const runSimonTurn = (simonSTurn: boolean, r: number) => {
+    const simonSeq = [];
     if (simonSTurn == true) {
       for(let i=0; i<r; i++){
-
-        console.log(randSeq[r]);
+        simonSeq.push(randSeq[i])
+        setTimeout(() => {
+          if (randSeq[i] === 1) {
+            setIsLitOne(true);
+            setTimeout(() => setIsLitOne(false), 500);  // Flash duration
+          }
+          if (randSeq[i] === 2) {
+            setIsLitTwo(true);
+            setTimeout(() => setIsLitTwo(false), 500);  // Flash duration
+          }
+          if (randSeq[i] === 3) {
+            setIsLitThree(true);
+            setTimeout(() => setIsLitThree(false), 500);  // Flash duration
+          }
+          if (randSeq[i] === 4) {
+            setIsLitFour(true);
+            setTimeout(() => setIsLitFour(false), 500);  // Flash duration
+          }
+        }, i * 1000); 
       }
     }
+    setPlayerTurn(!playerTurn)
+    console.log(simonSeq)
+    return simonSeq
   };
 
+
+
+  const runPlayerTurn = (playerTurn:boolean, r :number, s:number[])=>{
+    if (playerTurn == true) {
+    //   for(let i=0; i<r; i++){
+        
+    // }
+  }
+}
+
+
+
   const clickedOne = (i: number) => {
-    console.log(i);
+    return i
   };
 
   const clickedTwo = (i: number) => {
-    console.log(i);
+    setIsLitTwo((prevState) => !prevState);
+    setTimeout(()=>(setIsLitTwo((prevState) => !prevState)),1000);
+    return i
   };
 
   const clickedThree = (i: number) => {
-    console.log(i);
+    setIsLitThree((prevState) => !prevState);
+    setTimeout(()=>(setIsLitThree((prevState) => !prevState)),1000);
+    return i
   };
 
   const clickedFour = (i: number) => {
-    console.log(i);
+    setIsLitFour((prevState) => !prevState);
+    setTimeout(()=>(setIsLitFour((prevState) => !prevState)),1000);
+    return i
   };
 
   return (
@@ -132,18 +191,18 @@ function App() {
           <div>
             <p>Highscore: {highscore}</p>
           </div>
-          <Button onClick={() => clickedOne(1)}>1</Button>
+          <Button isLit={isLitOne} onClick={() => clickedOne(1)}>1</Button>
           <div></div>
-          <Button onClick={() => clickedTwo(2)}>2</Button>
+          <Button isLit={isLitTwo} onClick={() => clickedTwo(2)}>2</Button>
           <Scoreboard>
             <p>Score: {score}</p>
             {hideStart && (
               <Start onClick={startGame}>{hideStart ? "Start" : ""}</Start>
             )}
           </Scoreboard>
-          <Button onClick={() => clickedThree(3)}>3</Button>
+          <Button isLit={isLitThree} onClick={() => clickedThree(3)}>3</Button>
           <div></div>
-          <Button onClick={() => clickedFour(4)}>4</Button>
+          <Button isLit={isLitFour} onClick={() => clickedFour(4)}>4</Button>
         </Pad>
         <Footer>By: Khoi</Footer>
       </Layout>
